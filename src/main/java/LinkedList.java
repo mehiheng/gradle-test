9f130d06519dc78656783f734eb2736d4bb85394
ZHOUAD <Jing690518>
2018-07-14 13:35:15 +0800

    gradle test

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,7 @@
+import javafx.beans.value.ObservableBooleanValue;
+
+public class LinkedList {
+    public String get(int i){
+        return "first";
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class test {
+    @Test
+    public void testMockClass() throws Exception {
+        // you can mock concrete classes, not only interfaces
+        LinkedList mockedList = mock(LinkedList.class);
+
+        // stubbing appears before the actual execution
+        String value = "first";
+        Mockito.when(mockedList.get(0)).thenReturn(value);
+
+        assertEquals(mockedList.get(0), value);
+
+    }
+}
